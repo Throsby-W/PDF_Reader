5140902697c71bb467794d428d1593fd3b4d12b2
Throsby <throsbywells@gmail.com>
2021-01-25 14:48:15 -0500

    Supported multiple tables on one page
    Cleared old comments, put in for loop to go through tables on each page of pdf

--- pdfreader.py
+++ pdfreader.py
@@ -2,11 +2,13 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 import camelot, os
 import pandas as pd
 
-test_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/rkcall.pdf"
-fit_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/fitting20.pdf"
+test_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/temp0.pdf"
+fit_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/working211.pdf"
 shoot_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/shooting.pdf"
 
 pdf = PdfFileReader(open(test_path,'rb'))
+
+
 
 ### Saves the pages of the input pdf to the
 for page in range(pdf.numPages):
@@ -16,25 +18,15 @@ for page in range(pdf.numPages):
         output.write(outputStream)
 
 pdf_tables = pd.DataFrame()
-pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_rows', None)
 ###Turn the temporary pdfs into TableList objects and delete the temp pdfs
 for page in range(pdf.numPages):
     working_on = "/Users/Throsby/Desktop/PDF Reader Project/temp%s.pdf"%(page)
     sheet = camelot.read_pdf(working_on, flavor='lattice', line_scale=60)
-    pdf_tables = pdf_tables.append(sheet[0].df)
+    for table in range(0,sheet.n):
+        pdf_tables = pdf_tables.append(sheet[table].df)
     os.remove(working_on)
 print(pdf_tables)
 
 
 pdf_tables.to_csv("output.csv")
-# frame = pd.DataFrame()
-# for i in len(pdf_tables):
-#     frame.append(pdf_tables[i-1])
-
-
-#
-# finalize = PdfFileWriter()
-# finalize.addPage()
-#
-#
-# .export('testout.csv',f='csv',compress=False)
