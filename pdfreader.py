3cf463a57f9ee93c9a6fbac79a4f86a7ef74ce69
Throsby Wells <throsbywells@gmail.com>
2021-04-06 19:03:39 -0400

    Update
    Pretty big update that i really should have made in more incremental commits. Terminal interaction and sass rather than opening the file and editing the file path string, cycles through the files saved as pdfs assuming the correct naming convention. Creates the processed csv. Sends to google sheets as pandas dataframe. Opens browser with corresponding google sheet. Opens the corresponding pdf in Adobe Acrobat.

--- pdfreader.py
+++ pdfreader.py
@@ -1,32 +1,74 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
-import camelot, os
+import camelot, os, sys, os.path
+from os import path
 import pandas as pd
+import sheets_pdfreader
+import subprocess, webbrowser
 
-test_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/temp0.pdf"
-fit_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/working211.pdf"
-shoot_path = "/Users/Throsby/Desktop/PDF Reader Project/Test PDFs/shooting.pdf"
+try:
+    date_prefix = sys.argv[1]
+except IndexError:
+    date_prefix = input("\nWow\nThat was an early mistake! Did you also for get to download the files today too?\n\nWhat's the prefix du jour? ")
 
-pdf = PdfFileReader(open(test_path,'rb'))
+def csvize(file_path_in):
+
+    file_path = "/Users/Throsby/Documents/GitHub/PDF_Reader/Test PDFs/{}{}ing.pdf".format(date_prefix,file_path_in)
+    ###Handle the various ways that I might forget to include the date ###
+    try:
+        f = open(file_path,'rb')
+        pdf = PdfFileReader(f)
+    except FileNotFoundError:
+        try:
+            corrected_date_prefix = input("File not found, what date did you forget? ")
+            if(corrected_date_prefix=="None"):
+                pass
+            file_path = "/Users/Throsby/Documents/GitHub/PDF_Reader/Test PDFs/{}{}ing.pdf".format(date_prefix,file_path_in)
+            f = open(file_path,'rb')
+            pdf = PdfFileReader(f)
+        except FileNotFoundError:
+            corrected_date_prefix = input("\nGet it together, bud.\nLast shot!\n\nFile not found, what date did you mean? ")
+            file_path = "/Users/Throsby/Documents/GitHub/PDF_Reader/Test PDFs/{}{}ing.pdf".format(date_prefix,file_path_in)
+            f = open(file_path,'rb')
+            pdf = PdfFileReader(f)
 
 
+    ### Creates name of file without filetype as tail_wo_filetype. Creates procedure as the first four letters of tail_wo_filetype
+    head, tail_with_filetype = os.path.split(file_path)
+    tail_wo_filetype = os.path.splitext(tail_with_filetype)[0]
+    procedure = tail_wo_filetype[:4]
 
-### Saves the pages of the input pdf to the
-for page in range(pdf.numPages):
-    output = PdfFileWriter()
-    output.addPage(pdf.getPage(page))
-    with open("temp%s.pdf"%(page),"wb") as outputStream:
-        output.write(outputStream)
+    ### Saves the pages of the input pdf to the
+    for page in range(pdf.numPages):
+        output = PdfFileWriter()
+        output.addPage(pdf.getPage(page))
+        with open("temp%s.pdf"%(page),"wb") as outputStream:
+            output.write(outputStream)
 
-pdf_tables = pd.DataFrame()
-# pd.set_option('display.max_rows', None)
-###Turn the temporary pdfs into TableList objects and delete the temp pdfs
-for page in range(pdf.numPages):
-    working_on = "/Users/Throsby/Desktop/PDF Reader Project/temp%s.pdf"%(page)
-    sheet = camelot.read_pdf(working_on, flavor='lattice', line_scale=60)
-    for table in range(0,sheet.n):
-        pdf_tables = pdf_tables.append(sheet[table].df)
-    os.remove(working_on)
-print(pdf_tables)
+    pdf_tables = pd.DataFrame()
+    f.close()
+    pd.set_option('display.max_rows', None)
+
+    ###Turn the temporary pdfs into TableList objects and delete the temp pdfs
+    for page in range(pdf.numPages):
+        working_on = "/Users/Throsby/Documents/GitHub/PDF_Reader/temp%s.pdf"%(page)
+        sheet = camelot.read_pdf(working_on, flavor='lattice', line_scale=60)
+
+        for table in range(0,sheet.n):
+            pdf_tables = pdf_tables.append(sheet[table].df)
+        os.remove(working_on)
+    print(pdf_tables)
+
+    pdf_tables.to_csv("%s.csv"%(tail_wo_filetype), index=False)
+    print("%s.csv Done!"%(tail_wo_filetype))
 
 
-pdf_tables.to_csv("output.csv")
+for file in ["work","test","fitt"]:
+    pathstring = "/Users/Throsby/Documents/GitHub/PDF_Reader/Test PDFs/{}{}ing.pdf".format(date_prefix,file)
+    if(path.exists(pathstring)):
+        csvize(file)
+        fileydo = "/Users/Throsby/Documents/GitHub/PDF_Reader/Test PDFs/{}{}ing.pdf".format(date_prefix,file)
+        runner = subprocess.Popen(['open','-a','Adobe Acrobat Reader DC',fileydo], shell=False)
+        sheets_pdfreader.make_sheet_up(pathstring)
+    else:
+        print("{}{}ing.pdf doesn't exist!".format(date_prefix,file))
+        continue
